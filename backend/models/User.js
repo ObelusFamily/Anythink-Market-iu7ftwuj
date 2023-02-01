a35f5bab0fd80e1b331fc8b19afa4127fe24c549
Ashwini V <92977685+ashwiniv6@users.noreply.github.com>
2023-02-01 07:02:45 +0000

    isVerified

--- backend/models/User.js
+++ backend/models/User.js
@@ -33,6 +33,7 @@ var UserSchema = new mongoose.Schema(
       type: Boolean,
       default: false
     },
+    
     favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: "Item" }],
     following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     hash: String,
